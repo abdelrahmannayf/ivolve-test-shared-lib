404250b5306dc9c37accaa28e2070daaa48a27e8
abdelrahmannayf <abdelrahmannayf@gmail.com>
2026-02-19 21:30:03 +0200

    Update scanImage.groovy

--- vars/scanImage.groovy
+++ vars/scanImage.groovy
@@ -1,4 +1,12 @@
 def call(String imageName, String tag) {
+
     echo "Scanning Image with Trivy..."
-    sh "trivy image ${imageName}:${tag}"
+
+    sh """
+        trivy image \
+        --scanners vuln \
+        --timeout 10m \
+        --severity HIGH,CRITICAL \
+        ${imageName}:${tag}
+    """
 }
